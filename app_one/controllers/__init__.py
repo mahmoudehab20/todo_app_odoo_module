f0b8273f037da6d3e772694ede3c2a90fca198e1
mahmoudehab20 <118760749+mahmoudehab20@users.noreply.github.com>
2024-12-08 14:42:00 +0200

    Delete app_one directory

--- app_one/controllers/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import property_api
-from . import test_api
-
--- app_one/controllers/test_api.py
+++
@@ -1,9 +0,0 @@
-from odoo import http
-
-
-class TestApi(http.Controller):
-
-    @http.route("/api/test", methods=["GET"], type="http", auth="none", csrf=False)
-    def test_endpoint(self):
-        print('inside test_endpoint method')
-    
